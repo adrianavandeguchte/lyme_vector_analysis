4e750f423c5763695cd1f6d79cba4d25bde91bfc
adrianavandeguchte <61213204+adrianavandeguchte@users.noreply.github.com>
2020-07-05 20:34:01 -0400

    Update app.py
    added code from branch app.py to prevent conflict merge

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ import sqlalchemy
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify, render_template
 from config import password
+from dbFunction import *
+
+#Take care of page not found
+@app.errorhandler(404)
 
 app = Flask(__name__)
+
+# Use inbuilt function which takes error as parameter
+def not_found(e):
+  #redirect to home page - we can always show custom page
+  return render_template("index.html", title="Lyme Vector Analysis")
 
 # Connect to Database,
 engine = create_engine(f'postgresql://hehyvkiqvzdmhq:{password}@ec2-52-0-155-79.compute-1.amazonaws.com:5432/dchplfe6pe6n1h')
@@ -29,29 +38,13 @@ def home():
     data = [tick_data, lyme_data]
     return render_template("index.html", data=data)
 
-
-
-# @app.route("/tick_data")
-# def tick_data():
-#     result_set = engine.execute("select fips_code, COALESCE(county_status,'No records') from tickstatus")
-#     tick_list = []
-#     for r in result_set:
-#         row = [r[0],r[1]]
-#         tick_list.append(row)
-#         tick_data = {'loc': tick_list}
-#     return jsonify(tick_data)
-#
-# @app.route("/lyme_data")
-# def lyme_data():
-#     result_set = engine.execute("select concat(substring(to_char(st_cty_code, 'FM999999') from 1 for 1),substring(to_char(st_cty_code, 'FM999999') from 2 for 3)),cases from lymenation where state in ('AL','AK','AZ','AR','CA','CO','CT') union select concat(substring(to_char(st_cty_code, 'FM999999') from 1 for 2),right(concat('0',substring(to_char(st_cty_code, 'FM999999') from 3 for 3)),3)),cases from lymenation where state not in ('AL','AK','AZ','AR','CA','CO','CT')")
-#     lyme_list = []
-#     for r in result_set:
-#         row = [r[0],r[1]]
-#         lyme_list.append(row)
-#     lyme_data = {'loc': lyme_list}
-#     return jsonify(lyme_data)
-
-
+@app.route("/api/v1.0/deerpopLyme")
+def deerpopLyme():
+    #Get deerpopLyme results
+    deerLymen_dict = fDeerpopLymeCount()
+    print(deerLymen_dict)
+    #Return json output
+    return deerLymen_dict
 
 
 @app.route("/lymedogs")
